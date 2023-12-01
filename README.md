6622f08750928864f09484dcf2bd10be0773289e
Jakub Bełch <jakub.belch@detable.pl>
2023-12-01 11:32:26 +0100

    update settings description

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Obsidian Plugin - Multi State CheckBox Switcher
-It works with theme eg.`Things` which presents states with pretty icons
+It works with theme eg. [Things](https://github.com/colineckert/obsidian-things) which presents states with pretty icons
 
 This is a plugin which handle with 3-state checkbox
 - [ ] unchecked
 - [/] partially done 
 - [x] done
 
-and multistate checkboxes (customizable) with defaults:
+and multistate checkboxes (customizable) with defaults [Quick Reference](https://github.com/colineckert/obsidian-things/blob/main/assets/checkbox-styles.png):
 - [!] 
 - [?]
 - [i]
@@ -17,3 +17,5 @@ and multistate checkboxes (customizable) with defaults:
 - [k]
 - [u]
 - [d]
+
+![Quick Reference](/resources/checkbox-styles.png)
--- src/SettingsTab.ts
+++ src/SettingsTab.ts
@@ -41,7 +41,42 @@ export class SampleSettingTab extends PluginSettingTab {
         const { containerEl } = this;
 
         containerEl.empty();
+        const fragment = document.createDocumentFragment();
+		containerEl.createEl('h1', { text: 'Multi State CheckBox Switcher' });
 
+		const descriptionEl = containerEl.createEl('div');
+        descriptionEl.createEl("p").append(
+            "This plugin allows toggles 3 state intead of default two, it works nad looks perfectly when theme supports it eg. ",
+            fragment.createEl('a', {
+				href: 'https://github.com/colineckert/obsidian-things',
+				text: 'Things',
+			}),
+        );
+
+        ///primaryStatesDescEl
+        const primaryStatesDescEl = descriptionEl.createEl('div', {
+			cls: 'pattern-defaults',
+		});
+		primaryStatesDescEl.createEl('h3', { text: `Primary states` });
+
+        const descriptionStatesEl = primaryStatesDescEl.createEl('ul');
+		descriptionStatesEl.createEl("li").append('empty: "- [ ] " ');
+		descriptionStatesEl.createEl("li").append('partial: "- [/] " ');
+		descriptionStatesEl.createEl("li").append('checked: "- [x] " ');
+
+        ///additionalStatesDescEl
+        const additionalStatesDescEl = descriptionEl.createEl('div', {
+			cls: 'pattern-defaults',
+		});
+		additionalStatesDescEl.createEl('h3', { text: `Additional states` });
+        additionalStatesDescEl.createEl("p").append(
+            fragment.createEl('a', {
+				href: 'https://github.com/colineckert/obsidian-things/blob/main/assets/checkbox-styles.png',
+				text: 'Quick reference',
+			}),
+        );
+
+        ///additionalStatesSettings
         this.plugin.settings.AdditionalStates.forEach((state, idx) => {
             new Setting(containerEl)
                 .setName("Additional state #" + idx)
